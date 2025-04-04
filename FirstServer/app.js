99a9c2608aca9c545f9268e832cf5a3cc7aaf72c
Swati Pal <swatipal39@gmail.com>
2025-04-04 12:26:35 +0530

    Taken out the common lines of code from response of each url and placed outside

--- FirstServer/app.js
+++ FirstServer/app.js
@@ -4,28 +4,21 @@ const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
     //process.exit();         //Stops event loop. Logs the 1st request received by server, logs it and then shuts down the server.
 
-
-    if(req.url==='/'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Webpage</title></head>');
-        res.write('<body><h1>Welcome to our homepage!</h1></body>');
-        res.write('</html>');   
-        return res.end();
-    }else if(req.url==='/products'){
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<head><title>My First Webpage</title></head>');
-        res.write('<body><h1>Check our latest collection of products!!</h1></body>');
-        res.write('</html>');   
-        return res.end();
-    }
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>My First Webpage</title></head>');
+
+    if(req.url==='/'){
+        res.write('<body><h1>Welcome to our homepage!</h1></body>');
+        return res.end();
+    }else if(req.url==='/products'){  
+        res.write('<body><h1>Check our latest collection of products!!</h1></body>');
+        return res.end();
+    }
+
     res.write('<body><h1>Like / Share / Subscribe</h1></body>');
     res.write('</html>');   
-    res.end();
+    return res.end();
 });
 
 const PORT = 3000;

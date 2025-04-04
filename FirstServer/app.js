fd455ba727a9a9f3f1b561e8c60e2567b7da418f
Swati Pal <swatipal39@gmail.com>
2025-04-04 12:09:51 +0530

    Added the response to be returned to client when a request is made to specific URL

--- FirstServer/app.js
+++ FirstServer/app.js
@@ -2,7 +2,30 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
     console.log(req.url, req.method, req.headers);
-    process.exit();         //Stops event loop. Logs the 1st request received by server, logs it and then shuts down the server.
+    //process.exit();         //Stops event loop. Logs the 1st request received by server, logs it and then shuts down the server.
+
+
+    if(req.url==='/'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Webpage</title></head>');
+        res.write('<body><h1>Welcome to our homepage!</h1></body>');
+        res.write('</html>');   
+        return res.end();
+    }else if(req.url==='/products'){
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My First Webpage</title></head>');
+        res.write('<body><h1>Check our latest collection of products!!</h1></body>');
+        res.write('</html>');   
+        return res.end();
+    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My First Webpage</title></head>');
+    res.write('<body><h1>Like / Share / Subscribe</h1></body>');
+    res.write('</html>');   
+    res.end();
 });
 
 const PORT = 3000;

55b97053afbe695b27ff690361dbf97ec9763a91
Swati Pal <swatipal39@gmail.com>
2025-04-08 17:20:05 +0530

    When browser is hit to see the output app.use displays by skipping app.post because we haven't made POST request

--- Express/app.js
+++ Express/app.js
@@ -1,5 +1,5 @@
 //Core Module
-const http = require('http');
+//const http = require('http');
 
 //External Module
 const express = require('express');
@@ -9,20 +9,21 @@ const reqHandler = require('./user');
 
 
 const app = express();
+
 //Middleware
-app.use((req,res,next)=>{
-  console.log("Came in first middleware: ",req.url, req.method);
-  next();
-});
-app.use((req,res,next)=>{
+app.post("/submit-details",(req,res,next)=>{                //Route matches based on HTTP request
   console.log("Came in second middleware: ",req.url, req.method);
   res.send('<p>Welcome to my HomePage.</p>');
 })
+app.use("/",(req,res,next)=>{                               //Route matches based on path(URL)
+  console.log("Came in first middleware: ",req.url, req.method);
+});
 
-const server = http.createServer(app);
+
+//const server = http.createServer(app);  We will now create server using Express
 
 const PORT = 3000;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server running on address http://localhost:${PORT}`);
 });
 

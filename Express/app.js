dc8ce7ca7e1a8cac697fccd1dc5ace8866cb70cc
Swati Pal <swatipal39@gmail.com>
2025-04-08 17:24:09 +0530

    Added routes that runs based on HTTP request

--- Express/app.js
+++ Express/app.js
@@ -11,13 +11,18 @@ const reqHandler = require('./user');
 const app = express();
 
 //Middleware
-app.post("/submit-details",(req,res,next)=>{                //Route matches based on HTTP request
+app.get("/",(req,res,next)=>{                               //Route matches based on HTTP request
+  console.log("Came in first middleware: ",req.url, req.method);
+  next();
+});
+app.post("/submit-details",(req,res,next)=>{          //Route matches based on HTTP request. Only matches with complte route '/submit-details'
   console.log("Came in second middleware: ",req.url, req.method);
   res.send('<p>Welcome to my HomePage.</p>');
-})
-app.use("/",(req,res,next)=>{                               //Route matches based on path(URL)
-  console.log("Came in first middleware: ",req.url, req.method);
 });
+app.use("/",(req,res,next)=>{        //Route matches based on path(URL). Use works for all wildcard paths i.e. matches for all paths having '/' in their path
+  console.log("Came in another middleware: ",req.url, req.method);
+});
+
 
 
 //const server = http.createServer(app);  We will now create server using Express

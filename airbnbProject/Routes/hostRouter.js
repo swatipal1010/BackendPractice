033c6075f3255336faf225d2b1317520929ca0e7
Swati Pal <swatipal39@gmail.com>
2025-04-10 23:19:53 +0530

    Used absolute paths for the HTML files that serves the content on hitting a particular URL

--- airbnbProject/Routes/hostRouter.js
+++ airbnbProject/Routes/hostRouter.js
@@ -1,23 +1,18 @@
+const path = require('path');
+
 const express = require('express');
 const hostRouter = express.Router();
+
+const rootDir = require('../utils/pathUtils');
 
 
 hostRouter.get("/add-home",(req,res,next)=>{
     console.log(req.url, req.method);
-    res.send(`
-        <h1>Register your home here.</h1>
-        <form action="/host/add-home" method="POST">
-        <input type="text" name="houseName" placeholder="Enter the name of your house."/>
-        <input type="submit">
-        </form>
-    `);
+   res.sendFile(path.join(rootDir, 'views','addHome.html'));
 });
 
 hostRouter.post("/add-home",(req,res,next)=>{
-    res.send(`
-        <h1>Home added succesfully.</h1>
-        <a href='/'>Go to Home.</a>
-    `);
+    res.sendFile(path.join(rootDir, 'views','homeAdded.html'));
 });
 
 module.exports = hostRouter;
--- airbnbProject/Routes/userRouter.js
+++ airbnbProject/Routes/userRouter.js
@@ -1,11 +1,13 @@
+const path = require('path');
+
 const express = require('express');
 const userRouter = express.Router();
 
+const rootDir = require('../utils/pathUtils');
+
 userRouter.get("/",(req,res,next)=>{
     console.log(req.url, req.method);
-    res.send(`<h1>Welcome to airbnb!!</h1>
-        <a href='/host/add-home'>Register your home on airbnb.</a>
-    `);
+    res.sendFile(path.join(rootDir, 'views','home.html'));
 });
 
 module.exports = userRouter;

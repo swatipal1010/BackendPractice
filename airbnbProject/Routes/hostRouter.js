92c37a44da71b10df5b7fb55522ba25496a522b8
Swati Pal <swatipal39@gmail.com>
2025-04-10 12:17:04 +0530

    Path handling of URLs for host

---
+++ airbnbProject/Routes/hostRouter.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const hostRouter = express.Router();
+
+
+hostRouter.get("/host/add-home",(req,res,next)=>{
+    console.log(req.url, req.method);
+    res.send(`
+        <h1>Register your home here.</h1>
+        <form action="/host/add-home" method="POST">
+        <input type="text" name="houseName" placeholder="Enter the name of your house."/>
+        <input type="submit">
+        </form>
+    `);
+});
+
+hostRouter.post("/host/add-home",(req,res,next)=>{
+    res.send(`
+        <h1>Home added succesfully.</h1>
+        <a href='/'>Go to Home.</a>
+    `);
+});
+
+module.exports = hostRouter;
